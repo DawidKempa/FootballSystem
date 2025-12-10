8b4065c32e1665f770397faeb1717ceec4df9d50
DawidKempa <kempadawid1@wp.pl>
2025-12-10 02:35:36 +0100

    Dodanie pustego projektu react + ts + tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="py-10 h-screen">
+      <h1 className="font-bold">Hello Football System Frontend!</h1>
+    </main>
+  );
+}
+
+export default App;
